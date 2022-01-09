d614241f70d7bc992e51ff98feee3e28c2b9725b
prashant040391 <88234389+prashant040391@users.noreply.github.com>
2022-01-09 21:17:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=553397529585.dkr.ecr.us-east-1.amazonaws.com/prashant_course_assignment:latest
-docker system prune
-docker pull $DOCKER_IMAGE
-docker rm -f prashant
-docker run -itd -p 8080:8081 --name=prashant $DOCKER_IMAGE
+sudo docker system prune
+sudo docker pull $DOCKER_IMAGE
+sudo docker rm -f prashant
+sudo docker run -itd -p 8080:8081 --name=prashant $DOCKER_IMAGE
