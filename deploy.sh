15ce22d1f0479710028a3a56ff2c20bec7435711
prashant040391 <88234389+prashant040391@users.noreply.github.com>
2022-01-09 21:13:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=553397529585.dkr.ecr.us-east-1.amazonaws.com/prashant_course_assignment:latest
 docker pull $DOCKER_IMAGE
-docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -itd -p 8080:8081 $DOCKER_IMAGE
+a=`docker ps | wc -l`; if [ $a != 1 ];then docker rm -f prashant;fi
+docker rm -f prashant
+docker run -itd -p 8080:8081 --name=prashant $DOCKER_IMAGE
