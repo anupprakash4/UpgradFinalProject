8637709dfc71dd4efae5493fabd1428e0b080933
anupprakash4 <88568799+anupprakash4@users.noreply.github.com>
2022-01-09 22:59:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Starting to deploy docker image.."
-DOCKER_IMAGE=553397529585.dkr.ecr.us-east-1.amazonaws.com/prashant_course_assignment:latest
+DOCKER_IMAGE=597824617530.dkr.ecr.us-east-1.amazonaws.com/anoop-favorite-ecr:latest
 sudo docker system prune
 sudo docker pull $DOCKER_IMAGE
-sudo docker rm -f prashant
-sudo docker run -itd -p 8080:8081 --name=prashant $DOCKER_IMAGE
+sudo docker rm -f Anoop
+sudo docker run -itd -p 8080:8081 --name=Anoop $DOCKER_IMAGE
